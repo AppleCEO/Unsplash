ae73d364d2759ef575327025945312a35adf7c79
도미닉 <kiljh@me.com>
2026-02-06 22:42:59 +0900

    첫 화면을 AppCoordinator를 이용해 이동하도록 수정

--- Unsplash/Scenes/AppCoordinator.swift
+++ Unsplash/Scenes/AppCoordinator.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import ReactorKit
 
 class AppCoordinator: Coordinator {
     var parentCoordinator: Coordinator? = nil
     var childCoordinators = [Coordinator]()
     private let window: UIWindow?
     
+    lazy var navigationController: UINavigationController = {
+        let searchViewController = SearchViewController()
+        searchViewController.reactor = SearchViewReactor()
+        let navigationController = UINavigationController(rootViewController: searchViewController)
+        return navigationController
+    }()
+    
     init(window: UIWindow?) {
         self.window = window
+    }
+    
+    func start() {
+        guard let window else {
+            return
+        }
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
     }
 }
