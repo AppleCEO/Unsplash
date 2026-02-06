bf34dea32a40c254aef3a46493f6f9cd26b41829
도미닉 <kiljh@me.com>
2026-02-06 22:12:45 +0900

    Coordinator를 사용하기 위해 폴더 구조 변경

---
+++ Unsplash/Scenes/Detail/DetailCoordinator.swift
@@ -0,0 +1,7 @@
+//
+//  DetailCoordinator.swift
+//  Unsplash
+//
+//  Created by 도미닉 on 2/6/26.
+//
+
---
+++ Unsplash/Scenes/Search/SearchCoordinator.swift
@@ -0,0 +1,7 @@
+//
+//  SearchCoordinator.swift
+//  Unsplash
+//
+//  Created by 도미닉 on 2/6/26.
+//
+
